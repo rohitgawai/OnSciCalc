a1e5b80dace0d8ebf9902460da3aa195ceda0836
rohitgawai <rgawai371@gmail.com>
2026-07-04 21:09:01 +0530

    delete  help drawer component and baseline site layout structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://onscicalc.com',
   devToolbar: {
     enabled: false,
   },
