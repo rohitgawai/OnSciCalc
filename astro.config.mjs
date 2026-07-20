30b3027f32a60c9a431310af7bae26f06657e5d8
rohitgawai <rgawai371@gmail.com>
2026-07-20 14:01:02 +0530

    new pages,

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://onscicalc.com',
+  trailingSlash: 'never',
 
   devToolbar: {
     enabled: false,
